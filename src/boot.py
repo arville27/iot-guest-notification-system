921461094f34d604f1b39f83e87cccfbb19d52d1
Daniel Wijaya <arville27@users.noreply.github.com>
2023-01-09 03:57:08 +0700

    Add extra info to LCD while connecting to network

--- src/boot.py
+++ src/boot.py
@@ -1,6 +1,7 @@
 # boot.py - - runs on boot-up
 
-from machine import sleep
+from machine import Pin, sleep, I2C
+from lib.i2c_lcd import I2cLcd
 import machine
 from config import (
     WIFI_SSID,
@@ -10,21 +11,41 @@ from config import (
     BROKER_PORT,
 )
 
+# I2C LCD display config
+I2C_ADDR = 0x27
+TOTAL_ROWS = 4
+TOTAL_COLUMNS = 20
+
+# GPIO Pin definition
+# SCL = D1, SDA = D2
+i2c = I2C(scl=Pin(5), sda=Pin(4), freq=10000)  # initializing the I2C method for ESP8266
+lcd = I2cLcd(i2c, I2C_ADDR, TOTAL_ROWS, TOTAL_COLUMNS)
+
 
 def do_connect(wifi_ssid: str, wifi_psk: str):
     import network
 
     wlan = network.WLAN(network.STA_IF)
     if not wlan.isconnected():
+        lcd_message = f"Connecting to WIFI  SSID: {WIFI_SSID}"
+        lcd.putstr(lcd_message)
         print("Connecting to network", end="")
         wlan.active(True)
         wlan.connect(wifi_ssid, wifi_psk)
+        dot_counter = 0
         while not wlan.isconnected():
             sleep(1000)
             print(".", end="")
+            if dot_counter == 3:
+                lcd.clear()
+                lcd.putstr(lcd_message)
+                dot_counter = 0
+            dot_counter += 1
+            lcd.putchar(".")
             pass
         print()
     print("Connected:", wlan.ifconfig())
+    lcd.clear()
 
 
 def connect_and_subscribe(hostname: str, port: int, client_id: str):
@@ -42,6 +63,7 @@ def restart_and_reconnect():
     machine.reset()
 
 
+lcd.clear()
 do_connect(WIFI_SSID, WIFI_PASSWORD)
 try:
     client = connect_and_subscribe(
--- src/main.py
+++ src/main.py
@@ -1,33 +1,30 @@
 # main.py
 
-from machine import Pin, sleep, I2C
-from lib.i2c_lcd import I2cLcd
 import urequests as requests
 from boot import client
 from config import BROKER_TOPIC_PUB, BROKER_TOPIC_SUB, LINE_ACCESS_TOKEN, LINE_GROUP_ID
+from lib.i2c_lcd import I2cLcd
+from machine import sleep, Pin, I2C
 
 # I2C LCD display config
 I2C_ADDR = 0x27
 TOTAL_ROWS = 4
 TOTAL_COLUMNS = 20
 
-# IR sensor config
-REQUIRED_DETECTION_TIME = 3  # in seconds
-HARD_LIMIT_TIME = REQUIRED_DETECTION_TIME * 10 + 5
-
-
-# GPIO Pin definition
-# SCL = D1, SDA = D2
 i2c = I2C(scl=Pin(5), sda=Pin(4), freq=10000)  # initializing the I2C method for ESP8266
 lcd = I2cLcd(i2c, I2C_ADDR, TOTAL_ROWS, TOTAL_COLUMNS)
 ir = Pin(13, Pin.IN)  # D7 (E18-D80NK, blue-GND, brown-VCC, black-OUT)
-led = Pin(2, Pin.OUT)  # D4 (built in LED)
+builtin_led = Pin(2, Pin.OUT)  # D4 (built in LED)
+
+# IR sensor config
+REQUIRED_DETECTION_TIME = 3  # in seconds
+HARD_LIMIT_TIME = REQUIRED_DETECTION_TIME * 10 + 5
 
 # Helper variable
 counter = 0
 right_to_send = True
 
-led.value(1)
+builtin_led.value(1)
 
 
 def send_message():
@@ -58,7 +55,7 @@ def action():
 
 
 def on_message_callback(topic, msg):
-    print(f"[{topic}] MQTT client received message: {msg.decode()}")
+    print(f"[{topic.decode()}] MQTT client received message: {msg.decode()}")
     lcd.clear()
     lcd.putstr(msg.decode())
 
@@ -77,10 +74,10 @@ while True:
 
     if right_to_send and counter >= REQUIRED_DETECTION_TIME * 10:
         right_to_send = False
-        led.value(0)
+        builtin_led.value(0)
         action()
 
     if not right_to_send and counter <= 15:
-        led.value(1)
+        builtin_led.value(1)
         right_to_send = True
         print("Ready to send another notification")
